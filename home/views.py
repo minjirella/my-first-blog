55169b402d898aa755f4b9a40ecd7b74a9249f93
minjirella <dyst0410@naver.com>
2019-01-31 17:10:32 +0900

    Studied CSS

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def index(request):
@@ -9,4 +10,5 @@ def index(request):
 
 
 def post_list(request):
-    return render(request, 'home/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'home/post_list.html', {'posts': posts})
